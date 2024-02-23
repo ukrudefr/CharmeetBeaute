2c0881dd52eef81756d0bd3883e2dde0a88cc72d
ukrudefr <ukrudefr@gmail.com>
2024-02-23 16:58:49 +0100

    offres

--- src/pages/Offers.js
+++ src/pages/Offers.js
@@ -4,7 +4,7 @@ import Line from '../components/Line.js'
 import offres1 from '../img_offres/soin_main.jpg'
 import offres2 from '../img_offres/Aqua_Romantica.jpg'
 import offres3 from '../img_offres/mains_vellours.jpg'
-import offres4 from '../img_offres/Promo_Hiver_WEBSITE.jpg'
+// import offres4 from '../img_offres/Promo_Hiver_WEBSITE.jpg'
 // import Snowfall from 'react-snowfall'
 
 export default function Offers() {
@@ -15,14 +15,14 @@ export default function Offers() {
 		<Line/>
 		{/* <Snowfall className="snowfall"/> */}
 		<Row>
-		<Col>
-		{/* <h3 style={{color: "#000", fontSize: "clamp(1.125rem, 0.875rem + 1.4286vw, 1.5rem)"}}>
-		SOINS de BEAUTÉ en Hiver</h3> */}
+		{/* <Col>
+		<h3 style={{color: "#000", fontSize: "clamp(1.125rem, 0.875rem + 1.4286vw, 1.5rem)"}}>
+		SOINS de BEAUTÉ en Hiver</h3>
 		<Image 
 		className="card"
 		src={offres4}
 		style={{width: "40rem", border: `none`}}
-		alt="card1"/></Col>
+		alt="card1"/></Col> */}
 		<Col><h3 style={{color: "#000", fontSize: "clamp(1.125rem, 0.875rem + 1.4286vw, 1.5rem)"}}>
 		Coffret soin des mains 24€</h3>
 		<Image 
